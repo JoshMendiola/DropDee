76b16a8c8c95f34a6e6093d5846bd162991d1d06
PhilosophyWithJosh <joshmendiola@icloud.com>
2023-02-02 22:29:21 -0600

    still simply playing with react at the moment

--- dropdee/src/App.js
+++ dropdee/src/App.js
@@ -1,17 +1,28 @@
-import React, {useState, useRef} from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import TodoList from './TodoList';
+import uuid from 'react-uuid'
 
-
+const LOCAL_STORAGE_KEY = 'DropDee.todos'
 function App() {
     const [todos, setTodos] = useState([])
     const todoNameRef = useRef()
+
+    useEffect(() => {
+        const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+        if(storedTodos) setTodos(storedTodos)
+    }, [])
+
+    useEffect(() => {
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+    },[todos])
+
 
     function handleAddTodo(e)
     {
         const name = todoNameRef.current.value
         if(name === '') return
         setTodos(prevTodos => {
-            return [...prevTodos, {id: 1, name: name, complete: false}]
+            return [...prevTodos, {id: uuid(), name: name, complete: false}]
         })
 
     }
