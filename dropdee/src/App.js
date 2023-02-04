f6407e8e7aeb3038c8e44244a6ac55f8ffbbfb1a
PhilosophyWithJosh <joshmendiola@icloud.com>
2023-02-04 13:31:59 -0600

    removes the login page and adds an app bar

--- dropdee/src/App.js
+++ dropdee/src/App.js
@@ -1,31 +1,11 @@
-import React, {useState, useRef, useEffect} from 'react';
-import uuid from 'react-uuid'
+import React from 'react';
+import MainAppBar from "./components/MainAppBar";
 
 
 function App() {
-    const employeeIDRef = useRef()
-    const employeePasswordRef = useRef()
-
-    function checkCredentials()
-    {
-        const employeeID = employeeIDRef.current.value
-        const employeePassword = employeePasswordRef.current.value
-
-        if (employeeID === '' || employeePassword === '') return
-
-        console.log(employeeID)
-        console.log(employeePassword)
-
-        employeeIDRef.current.value = null
-        employeePasswordRef.current.value = null
-    }
-
     return (
       <>
-          <p>Enter your employee ID and password to login</p>
-          <input type="text" ref = {employeeIDRef} placeholder = "Employee ID"/>
-          <input type = "password" ref = {employeePasswordRef} placeholder = "password"/>
-          <button onClick={checkCredentials}> Submit </button>
+          <MainAppBar/>
       </>
   )
 }
