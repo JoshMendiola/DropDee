d5f68da4c61ac8ef954fc9aa67b62fee69efe88f
PhilosophyWithJosh <joshmendiola@icloud.com>
2023-03-03 16:36:14 -0600

    changes app bar color upon login

--- dropdee/src/components/LoggedInAppBar.js
+++ dropdee/src/components/LoggedInAppBar.js
@@ -13,7 +13,7 @@ export default function LoggedInAppBar()
 
     return (
         <Box sx={{ flexGrow: 1 }}>
-            <AppBar position="static">
+            <AppBar position="static" style={{backgroundColor: 'maroon'}}>
                 <Toolbar>
                     <IconButton
                         size="large"
